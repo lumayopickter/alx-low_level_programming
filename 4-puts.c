48d05dc679798570842ce8717883e5ce125ae9a4
pickter lumayo <pickterlumayo@gmail.com>
2023-05-21 15:09:27 -0700

    Update 4-puts.c to print Programming is like building a multilingual puzzle

--- 4-puts.c
+++ 4-puts.c
@@ -4,15 +4,14 @@
  * main - Entry point of the program
  *
  * Description: This program prints the message
- * "\"Programming is like building a multilingual puzzle\""
- * without any additional punctuation using the puts function.
+ * "\"Programming is like building a multilingual puzzle"
+ * followed by a newline character using the puts function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-    return (0);
+	return (0);
 }
-
