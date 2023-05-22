4bfab4558ceefd30d23d80909ef7a3c9bd4b353d
pickter lumayo <pickterlumayo@gmail.com>
2023-05-22 15:01:15 -0700

    A 100-print combs 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
- *
- * Description: This program prints all possible different combinations of two digits.
- * Numbers are separated by ", " and printed in ascending order.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int first_digit;
-	int second_digit;
+	int num1, num2;
 
-	for (first_digit = 0; first_digit <= 9; first_digit++)
+	for (num1 = 0; num1 <= 8; num1++)
 	{
-		for (second_digit = first_digit + 1; second_digit <= 9; second_digit++)
+		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
-			putchar(first_digit + '0');
-			putchar(second_digit + '0');
-
-			if (first_digit != 8 || second_digit != 9)
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 != 8 || num2 != 9)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
 
