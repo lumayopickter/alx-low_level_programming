53b7c23ba8ff9b39cce6f8d1ee845eee12fcbf46
pickter lumayo <pickterlumayo@gmail.com>
2023-05-22 10:48:56 -0700

    A 8-printf base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all numbers of base 16 in lowercase
+ * without using the putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char hex_digits[] = "0123456789abcdef";
+    write(1, hex_digits, sizeof(hex_digits) - 1);
+    write(1, "\n", 1);
+
+    return 0;
+}
+
