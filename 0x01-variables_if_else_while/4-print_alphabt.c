81d0685ed4db9137f17cba3e10ae4930bd249959
pickter lumayo <pickterlumayo@gmail.com>
2023-05-22 11:28:30 -0700

    A 4-print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,22 +4,22 @@
  * main - Entry point
  *
  * Description: Prints the alphabet in lowercase excluding q and e
- * using the putchar function
+ *              using the putchar function
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        if (letter != 'q' && letter != 'e')
-            putchar(letter);
-    }
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
 
