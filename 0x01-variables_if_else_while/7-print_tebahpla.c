0ce706833b91ccc0b7ecee889c8c6423af4f67e3
pickter lumayo <pickterlumayo@gmail.com>
2023-05-22 09:40:27 -0700

    Add programme

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse
+ *              using the putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
+
