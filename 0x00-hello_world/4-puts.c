be27ca5e1d854fc77e576b1ca92d92beffbc2d41
pickter lumayo <pickterlumayo@gmail.com>
2023-05-22 13:38:51 -0700

    Add solution for 1-last_digit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
-int main(void) {puts("\"Programming is like building a multilingual puzzle"); return (0); }
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the message
+ * "\"Programming is like building a multilingual puzzle"
+ * followed by a newline character using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
