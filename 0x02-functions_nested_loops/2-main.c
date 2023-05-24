460bf9e7d3f605796f41ba54af8ff4042046da48
pickter lumayo <pickterlumayo@gmail.com>
2023-05-24 14:51:37 -0700

    A 2-alphabet x10 file

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,14 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
- */
-void print_alphabet_x10(void);
-
-/**
- * main - check the code
+ * main - check the code.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -12,6 +11,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
 
