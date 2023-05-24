58fa33ac43c1c101208124c4246a62d5cb624497
pickter lumayo <pickterlumayo@gmail.com>
2023-05-24 10:52:45 -0700

    A 6-abs file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs - Computes the absolute value of an integer
+ * @num: The number to compute the absolute value of
+ *
+ * Return: The absolute value of the number
+ */
+int _abs(int num)
+{
+	if (num < 0)
+		return -num;
+	else
+		return num;
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int num);
 
 #endif /* MAIN_H */
 
