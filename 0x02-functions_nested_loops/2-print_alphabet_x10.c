a15ffa2579c107a44d9ccca0d0be41e7837fd2ac
pickter lumayo <pickterlumayo@gmail.com>
2023-05-24 14:56:07 -0700

    A 2-alphabet x10 file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i;
+	int i;
+	char letter;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
 }
 
