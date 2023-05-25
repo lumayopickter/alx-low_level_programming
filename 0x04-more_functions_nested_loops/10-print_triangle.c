1903a78d784a74dcdb3adc0eed792ccfff9e6551
pickter lumayo <pickterlumayo@gmail.com>
2023-05-25 13:55:56 -0700

    A 10-print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,9 +18,9 @@ void print_triangle(int size)
 		return;
 	}
 
-	for (i = 1; i <= size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = 0; j <= i; j++)
 		{
 			_putchar('#');
 		}
