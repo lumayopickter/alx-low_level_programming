faae21370d34e5295e04a881fbe95af7b47a209b
pickter lumayo <pickterlumayo@gmail.com>
2023-05-25 02:46:17 -0700

    A debugging file in c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number = 0;
+
+	positive_or_negative(number);
+
+	return (0);
+}
+
