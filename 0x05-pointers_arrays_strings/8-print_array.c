1b4138d0bdf36df84a392ddfac9a676defa7af80
pickter lumayo <pickterlumayo@gmail.com>
2023-05-29 04:10:33 -0700

    A file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,20 +8,21 @@
  *
  * Description: This function prints the elements of an integer array
  *              up to the specified number, separated by commas and a space.
- *              The order of printed numbers is same as they are stored in array.
+ *              Order of printed numbers is same as they are stored in array.
  */
 void print_array(int *a, int n)
 {
-        int i;
+	int i;
 
-        for (i = 0; i < n; i++)
-        {
-                printf("%d", a[i]);
+	for (i = 0; i < n; i++)
+	{
 
-                if (i != n - 1)
-                        printf(", ");
-        }
+	printf("%d", a[i]);
 
-        printf("\n");
+	if (i != n - 1)
+	printf(", ");
+	}
+
+	printf("\n");
 }
 
