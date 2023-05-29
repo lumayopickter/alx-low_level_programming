f874d9bb6d719b9ad6bfbe635a61dabb0d58e083
pickter lumayo <pickterlumayo@gmail.com>
2023-05-29 03:41:10 -0700

    A print array file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints n elements of an array of integers
+ * @a: Pointer to the array of integers
+ * @n: Number of elements to print
+ *
+ * Description: This function prints the elements of an integer array
+ *              up to the specified number, separated by commas and a space.
+ *              The order of printed numbers is same as they are stored in array.
+ */
+void print_array(int *a, int n)
+{
+        int i;
+
+        for (i = 0; i < n; i++)
+        {
+                printf("%d", a[i]);
+
+                if (i != n - 1)
+                        printf(", ");
+        }
+
+        printf("\n");
+}
+
