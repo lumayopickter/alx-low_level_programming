cec35fea1dfaf7a3ff80673c74b52acd131f1c4f
pickter lumayo <pickterlumayo@gmail.com>
2023-05-31 09:03:08 -0700

    Rot 13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13 cipher.
+ * @str: The string to encode.
+ *
+ * Return: Pointer to the modified string.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; input[j] != '\0'; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
+
