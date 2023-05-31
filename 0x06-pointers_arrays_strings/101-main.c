0aceebd7f1bac3b224dad790f674ef6f2843005f
pickter lumayo <pickterlumayo@gmail.com>
2023-05-31 09:12:43 -0700

    print nimbers

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int _putchar(char c);
+
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
+}
+
