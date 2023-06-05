9bc81e4fee02c2ce03709a8fda15ab2cfa508df7
pickter lumayo <pickterlumayo@gmail.com>
2023-06-05 01:47:18 -0700

    pointer,strings and arrays

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Other function prototypes and includes */
+
+char *_memset(char *s, char b, unsigned int n);
+
+/* Other function prototypes and includes */
+
+#endif /* MAIN_H */
+
