b9096886e11ee553f3cd66e961dbe316e496f624
pickter lumayo <pickterlumayo@gmail.com>
2023-06-07 00:37:51 -0700

    Recursion

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Puts and recursionD
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Other function prototypes and declarations */
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
