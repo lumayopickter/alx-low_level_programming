5a30a10f682b0f68dbfe44d28364cb30ce732a9a
pickter lumayo <pickterlumayo@gmail.com>
2023-06-19 05:21:19 -0700

    puppy

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -2,17 +2,24 @@
 #include "dog.h"
 
 /**
- * main - check the code
+ * main - Entry point of the program
  *
- * Return: Always 0.
+ * Description: This program demonstrates the usage of the `struct dog` type.
+ *              It creates a dog named "Poppy" with an age of 3.5 years
+ *              and an owner named "Bob". It then prints the dog's name and age.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    struct dog my_dog;
+	struct dog my_dog;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+
+	return (0);
 }
+
