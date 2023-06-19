5339a9e5a039b0a2764b69359d1096201c0417d4
pickter lumayo <pickterlumayo@gmail.com>
2023-06-19 05:28:46 -0700

    Update dog.h with struct definition and include guards

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog (string)
+ * @age: Age of the dog (float)
+ * @owner: Owner of the dog (string)
+ *
+ * Description: This structure represents a dog with its name, age, and owner.
+ */
+struct dog
+{
     char *name;
     float age;
     char *owner;
