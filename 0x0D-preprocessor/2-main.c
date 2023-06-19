bc9961da21c22bab6a1467f7db16a7d05ea14d2e
pickter lumayo <pickterlumayo@gmail.com>
2023-06-19 05:03:43 -0700

    new file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the file it was compiled from.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
 
