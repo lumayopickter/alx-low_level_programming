f0c18835a35dcbd2c3e74366b9e4e6c9483df7c4
pickter lumayo <pickterlumayo@gmail.com>
2023-06-21 09:36:11 -0700

    functions

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,29 +5,29 @@
  * get_op_func - Selects the correct function to perform the operation.
  * @s: The operator passed as an argument.
  *
- * Return: A pointer to the function that corresponds to the operator given as a parameter.
- *         If the operator does not match any of the expected operators (+, -, *, /, %), return NULL.
+ * Return: A pointer function that correspond to operator given as parameter.
+ *         if operator not match expected operator(+, -, *, /, %), return NULL.
  */
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-    int i = 0;
+	int i = 0;
 
-    while (ops[i].op != NULL)
-    {
-        if (*(ops[i].op) == *s)
-            return (ops[i].f);
-        i++;
-    }
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s)
+			return (ops[i].f);
+		i++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,3 @@
-#include "3-calc.h"
-
 /**
  * op_add - Returns the sum of a and b.
  * @a: The first integer.
@@ -9,7 +7,7 @@
  */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
 /**
@@ -21,7 +19,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 
 /**
@@ -33,19 +31,19 @@ int op_sub(int a, int b)
  */
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 
 /**
- * op_div - Returns the division of a by b.
+ * op_div - Returns the result of the division of a by b.
  * @a: The first integer.
  * @b: The second integer.
  *
- * Return: The division of a by b.
+ * Return: The result of the division of a by b.
  */
 int op_div(int a, int b)
 {
-    return (a / b);
+	return (a / b);
 }
 
 /**
@@ -57,6 +55,6 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-    return (a % b);
+	return (a % b);
 }
 
