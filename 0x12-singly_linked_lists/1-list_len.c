cf7b6733bd19c6d17488266cd61d519664f82b34
pickter lumayo <pickterlumayo@gmail.com>
2023-07-02 22:21:54 -0700

    list lens

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a list_t list
+ * @h: A pointer to the head of the list
+ *
+ * Return: The number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	return (count);
+}
+
