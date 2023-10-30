4aebe67b9c41294c5d4fa40763f21147a5ea12ad
lumayopickter <pickterlumayo@gmail.com>
2023-10-30 13:50:18 -0700

    text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,58 +1,50 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output
- * @filename: Name of the file to read
- * @letters: Number of letters to read and print
- *
- * Return: The actual number of letters it could read and print.
- *         If the file can't be opened or read, or if filename is NULL, return 0.
- *         If write fails or does not write the expected amount of bytes, return 0.
+ * read_textfile - Reads and prints a text file to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ * Return: The actual number of letters read and printed. 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;              /* File descriptor */
-    ssize_t num_read;    /* Number of bytes read */
-    ssize_t num_written; /* Number of bytes written */
-    char *buffer;        /* Buffer for reading */
+	int file_descriptor;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
-    if (!filename) /* Check if filename is NULL */
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    /* Open the file for reading */
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
 
-    /* Allocate memory for the buffer */
-    buffer = malloc(letters);
-    if (!buffer)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
 
-    /* Read from the file */
-    num_read = read(fd, buffer, letters);
-    if (num_read == -1)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-    /* Write to standard output (stdout) */
-    num_written = write(STDOUT_FILENO, buffer, num_read);
-    if (num_written == -1 || num_written != num_read)
-    {
-        close(fd);
-        free(buffer);
-        return (0);
-    }
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-    close(fd);
-    free(buffer);
+	if (bytes_written == -1 || bytes_read != bytes_written)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-    return (num_written);
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_written);
 }
 
